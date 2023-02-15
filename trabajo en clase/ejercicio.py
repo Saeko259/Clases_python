52d23f58956d2fa8db2dd616743852f57daf0cdd
Saeko259 <Samueltellodiaz@gmail.com>
2023-02-14 20:16:37 -0500

    a

---
+++ trabajo en clase/ejercicio.py
@@ -0,0 +1,28 @@
+lista1 = ['hola','chao','tumadre']
+lista2 = []
+
+def agregar(
+    lista:list
+):
+    numero = int(input('Ingrese cuantos nombres quiere agregar: '))
+    for i in range(numero):
+        y = input(f'Ingrese el {i+1} nombre que quiere agregar: ')
+        lista.append(y)
+    print(lista)
+def eliminar(
+    listaa:list,
+    listaaa:list
+): 
+    numero = int(input('Digite cuantos usuarios quiere eliminar: '))
+    for i in range(numero): 
+        x = input(f'Escribe el nombre del usuario {i+1} que quieras eliminar: ')
+        listaaa.append(x)
+    for i in listaaa:
+        if i in listaa:
+            listaa.remove(i)
+    print(listaa)
+    listaaa.clear()
+    
+
+        
+
